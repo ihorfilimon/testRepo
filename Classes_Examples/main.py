5c797e0d003fef1bf0bf0ed14349a085621d4ea8
ihorfilimon <ihorfilimonchuk@gmail.com>
2018-12-29 23:09:52 +0200

    First classes

---
+++ Classes_Examples/main.py
@@ -0,0 +1,28 @@
+class Cat(object):
+    def __init__(self, date, age, height, length):
+        this.date = date
+        this.age = age
+        this.height = height
+        this.length = length
+
+        def change_date(self, date):
+            this.date = date
+
+        def change_age(self, age):
+            this.age = age
+
+        def change_height(self, height):
+            this.height = height
+
+        def change_length(self, length):
+            this.length = length
+
+
+class Cat_Pet(object):
+    def __init__(self, name, year, breed):
+        this.name = name
+        this.year = year
+        this.breed = breed
+
+        def change_name(self, name)
+            this.name = name
